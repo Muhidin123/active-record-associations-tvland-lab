c68b29ddc9834b31037dc05fd4eb7023c3270435
Muhidin <muhidinhukic1@gmail.com>
2020-11-06 15:57:06 -0600

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def get_roles
+        Character.all.select {|characters| characters.actor == self}
+    end
+
+
+    def list_roles
+        arr = get_roles.map {|character| character.name} << get_roles.map {|character| character.show.name}
+        arr.flatten.join(" - ")
+    end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+
+    def actors_list
+        Character.all.select {|chr| chr.show_id == self.id}.map {|show| show.actor.itself.full_name}
+    end
 end
+
